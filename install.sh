fec1fa2ba7da4631d387bfed26292a19a43de4ee
Roberto Torella <robertot@square-enix.com>
2016-01-15 16:25:07 +0000

    Fixed path issue in install that doesn't make the dotfiles installable via puppet

--- install.sh
+++ install.sh
@@ -3,35 +3,35 @@
 now=$(date +%Y%m%d-%H%M%S)
 
 echo "Creating backup files"
-[ -f ~/.zshrc ] && cp ~/.zshrc ~/.zshrc.orig.$now
-[ -f ~/.vimrc ] && cp ~/.vimrc ~/.vimrc.orig.$now
-[ -f ~/.tmux.conf ] && cp ~/.tmux.conf ~/.tmux.conf.orig.$now
-[ -f ~/.zcompdump ] && cp ~/.zcompdump ~/.zcompdump.orig.$now
+[ -f .zshrc ] && cp .zshrc .zshrc.orig.$now
+[ -f .vimrc ] && cp .vimrc .vimrc.orig.$now
+[ -f .tmux.conf ] && cp .tmux.conf .tmux.conf.orig.$now
+[ -f .zcompdump ] && cp .zcompdump .zcompdump.orig.$now
 
-[ -d ~/.dotfiles ] && mv ~/.dotfiles ~/.dotfiles.orig-$now
-[ -d ~/.oh-my-zsh ] && mv ~/.oh-my-zsh ~/.oh-my-zsh.orig.$now
-[ -d ~/.vim ] && mv ~/.vim ~/.vim.orig-$now
+[ -d .dotfiles ] && mv .dotfiles .dotfiles.orig-$now
+[ -d .oh-my-zsh ] && mv .oh-my-zsh .oh-my-zsh.orig.$now
+[ -d .vim ] && mv .vim .vim.orig-$now
 
 echo "Cloning dotfiles"
-git clone https://github.com/ganglio/dotfiles.git ~/.dotfiles
+git clone https://github.com/ganglio/dotfiles.git .dotfiles
 
 echo "Pulling submodules"
 OLD_PWD=$PWD
-cd ~/.dotfiles
+cd .dotfiles
 git submodule init
 git submodule update
 cd $OLD_PWD
 
 echo "Cloning oh-my-zsh"
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
 
 echo "Creating symlinks"
-ln -s ~/.dotfiles/zshrc ~/.zshrc
-ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/vim ~/.vim
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s .dotfiles/zshrc .zshrc
+ln -s .dotfiles/vimrc .vimrc
+ln -s .dotfiles/vim .vim
+ln -s .dotfiles/tmux.conf .tmux.conf
 
 # Initialising the autoupdate script
-date +%s > ~/.dotfiles/.lastupdated
+date +%s > .dotfiles/.lastupdated
 
 echo "Done! Happy commandlining :D"
